6d11c002a9df28bf4eb9c795decd2989e7c169ea
Omochice <mochice.mls.ntl@gmail.com>
2026-06-27 18:37:58 +0900

    test: add failing test for infra subcommand stub
    Extend the skeleton contract to the infra subcommand.

--- nyctereutes/cmd_test.go
+++ nyctereutes/cmd_test.go
@@ -29,3 +29,14 @@ func TestDepIsNotImplemented(t *testing.T) {
 		t.Errorf("want stderr to contain %q, got %q", "not implemented", stderr)
 	}
 }
+
+func TestInfraIsNotImplemented(t *testing.T) {
+	exit, _, stderr := run([]string{"infra"})
+
+	if exit != 1 {
+		t.Errorf("want exit status 1, got %d", exit)
+	}
+	if !strings.Contains(stderr, "not implemented") {
+		t.Errorf("want stderr to contain %q, got %q", "not implemented", stderr)
+	}
+}
