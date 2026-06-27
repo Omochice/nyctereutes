6e4ea95a963d738c138125caf3e59544b80ab893
Omochice <mochice.mls.ntl@gmail.com>
2026-06-27 18:36:58 +0900

    test: add failing test for dep subcommand stub
    Pin the skeleton contract: invoking dep must report it is not implemented and exit non-zero.

---
+++ nyctereutes/cmd_test.go
@@ -0,0 +1,31 @@
+package nyctereutes
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/Omochice/nyctereutes/cli"
+)
+
+func run(args []string) (exit int, stdout, stderr string) {
+	outBuf := &bytes.Buffer{}
+	errBuf := &bytes.Buffer{}
+	exit = MainCommand(args, &cli.ProcInout{
+		Stdin:  strings.NewReader(""),
+		Stdout: outBuf,
+		Stderr: errBuf,
+	})
+	return exit, outBuf.String(), errBuf.String()
+}
+
+func TestDepIsNotImplemented(t *testing.T) {
+	exit, _, stderr := run([]string{"dep"})
+
+	if exit != 1 {
+		t.Errorf("want exit status 1, got %d", exit)
+	}
+	if !strings.Contains(stderr, "not implemented") {
+		t.Errorf("want stderr to contain %q, got %q", "not implemented", stderr)
+	}
+}
