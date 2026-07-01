3560b671c48aa48bb7ced89ae579b6f8e266bd34
Omochice <mochice.mls.ntl@gmail.com>
2026-07-01 09:27:26 +0900

    fixup! refactor: drop redundant slash replace in EncodePath

--- internal/glab/path.go
+++ internal/glab/path.go
@@ -6,7 +6,7 @@ import "net/url"
 // `glab api` endpoint. The GitLab REST API addresses both projects
 // (`projects/:id`) and files (`repository/files/:file_path`) by a
 // namespace-qualified path in which the "/" separators must be escaped to %2F so
-// the whole value is treated as one path component. url.PathEscape already
+// the whole value is treated as one path component. [url.PathEscape] already
 // escapes "/" (it is reserved inside a path segment).
 func EncodePath(s string) string {
 	return url.PathEscape(s)
